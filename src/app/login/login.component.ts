61d75905c83b60a8eea55d3f9a32f5a032fad218
Menna Khalil <mennakhalilselim@gmail.com>
2024-03-22 12:47:50 +0200

    lab4

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  handleLogin(loginForm:any){
-    console.log(loginForm);
-    console.log(loginForm.value);
-    // console.log(loginEmail);
+  handleLogin(loginForm: any) {
+    if (!loginForm.valid) {
+      loginForm.control.markAllAsTouched();
+    }
   }
-
-  // handleEmail(loginEmail: any){
-  //   console.log(loginEmail);
-  // }
-
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.pattern('^[a-z0-9\.]{4,16}$')]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$')]],
+    })
+  }
+
+  handleRegister() {
+    console.log(this.registerForm);
+    if (!this.registerForm.valid) {
+      this.registerForm.markAllAsTouched();
+    }
+  }
 
 }
